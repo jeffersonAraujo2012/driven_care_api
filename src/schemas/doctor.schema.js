c8e7f363bb2b89d08d6eb6b23dcb8b1b00b35a07
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-04-01 16:13:05 -0300

    Update: date_of_birth field changed to age field

--- src/schemas/doctor.schema.js
+++ src/schemas/doctor.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const doctorSchema = Joi.object({
   name: Joi.string().required(),
-  dateOfBirth: Joi.date().required(),
+  age: Joi.number().min(0).max(120).required(),
   cpf: Joi.string().min(10).max(11).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
