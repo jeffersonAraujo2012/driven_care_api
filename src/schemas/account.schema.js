3356d9dcf6ff015f29e6f6b74926af3c4d48a85d
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-04-01 16:00:11 -0300

    Feat: errors in schemas

--- src/schemas/account.schema.js
+++ src/schemas/account.schema.js
@@ -1,7 +1,9 @@
 import Joi from "joi";
 
 const accountSchema = Joi.object({
-  email: Joi.email().required(),
+  email: Joi.string().email().required(),
   password: Joi.string().required(),
-  type: Joi.string().allow("patient", "doctor").required(),
+  type: Joi.string().valid("patient", "doctor").required(),
 });
+
+export default accountSchema;
--- src/schemas/doctor.schema.js
+++ src/schemas/doctor.schema.js
@@ -4,7 +4,7 @@ const doctorSchema = Joi.object({
   name: Joi.string().required(),
   dateOfBirth: Joi.date().required(),
   cpf: Joi.string().min(10).max(11).required(),
-  email: Joi.email().required(),
+  email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
 
