b90ddf67482145e6c5756d31710d6ee8b07f5208
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-04-03 17:37:36 -0300

    Update: added errors responses

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,18 +1,20 @@
 import jwt from "jsonwebtoken";
-import errors from "../errors";
+import errors from "../errors/index.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default async function authMiddleware(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
 
   try {
-    if (!token) throw errors.noTokenError();
-    jwt.verify(token, process.env.SECRET_KEY, (err, payload) => {
-      if (err) throw errors.unauthorizedError("Invalid or expired token");
+    if (!token) return res.status(401).send(errors.noTokenError());
+    jwt.verify(token, process.env.SECRET_JWT, (err, payload) => {
+      if (err) return res.status(401).send(errors.invalidTokenError());
       res.locals.user = payload;
+      next();
     });
-    next();
   } catch (err) {
-    next(err);
+    return res.sendStatus(500);
   }
 }
